29b748927757489f6f22a5bc02b82befdf30f961
Sanyam Jain <ersanyamjain@gmail.com>
2020-05-01 12:02:08 +0530

    Add files via upload

---
+++ J2EE/Servlet/Shopping Website1/WatchServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.sql.*;
+public class WatchServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
+	{
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		HttpSession session=req.getSession(false);
+		if(session!=null)
+		{
+			String watches[]=req.getParameterValues("watch");
+			session.setAttribute("watches",watches);
+			RequestDispatcher rd=req.getRequestDispatcher("/welcome.html");
+			rd.forward(req,res);
+		}
+		else
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("/index.html");
+			rd.forward(req,res);
+		}
+	}
+}
